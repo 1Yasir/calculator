4ad20109004ca1805a60dbf481b44111421cf51b
yasirmahmood <azmyasir250@gmail.com>
2024-02-02 19:54:51 +0500

    add script

--- js/app.js
+++ js/app.js
@@ -1,14 +1,47 @@
+let isfalse = false;
+// show result on click button 
+let btnClick = document.getElementById("buttonResult");
+// input use for get users Value 
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+
+// show the (hide/show) button on click  and remove and eventlistener base on condition input 
+
+function isHide(e){
+    e.preventDefault()
+    btnClick.classList.toggle("hide");
+    isfalse = !isfalse;
+    if(isfalse){
+        num1.removeEventListener("input", resultShow);   
+        num2.removeEventListener("input", resultShow);  
+    }else{
+        num1.addEventListener("input", resultShow);   
+        num2.addEventListener("input", resultShow);  
+    }
+    
+
+}
+
+// add result function add input and button 
+num1.addEventListener("input", resultShow)
+num2.addEventListener("input", resultShow)
+btnClick.addEventListener("click" , resultShow)
+
+
+
+
+// show result function 
 function resultShow() {
-    let user1 = Number(document.getElementById("num1").value.trim());
-    let user2 = Number(document.getElementById("num2").value.trim());
+    let user1 = Number(num1.value.trim());
+    let user2 = Number(num2.value.trim());
     let operator = document.getElementById("option").value;
     calculator(operator, user1, user2);
 }
 
+// simple calculator function 
 function calculator(operator = "+", a = 0, b = 0) {
+    // select the result element 
     const result = document.getElementById("result");
-   
-
     switch (operator) {
         case "+":
             result.innerHTML = (a + b);
@@ -31,3 +64,4 @@ function calculator(operator = "+", a = 0, b = 0) {
             result.innerHTML = ("wrong operator");
     }
 }
+
